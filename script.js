f017137cdb31f1ffab15b9c2c75e1d5d4185c6ed
Aamogh <120258212+aamoghS@users.noreply.github.com>
2023-09-01 22:24:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Add click event listener to dropdown menu items
+  var dropdowns = document.querySelectorAll(".dropdown");
+  for (var i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].addEventListener("click", function(event) {
+      var dropdownContent = this.querySelector(".dropdown-content");
+      // Close other open submenus
+      closeAllSubmenus();
+
+      // Toggle the display of the dropdown content
+      dropdownContent.classList.toggle("show");
+
+      // Prevent the click event from propagating to the window click handler
+      event.stopPropagation();
+    });
+
+    // Add mouseleave event listener to hide the submenu when not hovering
+    dropdowns[i].addEventListener("mouseleave", function() {
+      var dropdownContent = this.querySelector(".dropdown-content");
+      if (dropdownContent.classList.contains("show")) {
+        dropdownContent.classList.remove("show");
+      }
+    });
+  }
+
+  // Close submenus when clicking outside
+  window.addEventListener("click", function() {
+    closeAllSubmenus();
+  });
+
+  function closeAllSubmenus() {
+    var dropdownContents = document.querySelectorAll(".dropdown-content");
+    for (var i = 0; i < dropdownContents.length; i++) {
+      dropdownContents[i].classList.remove("show");
+    }
+  }
+});
 
 function updateClock() {
   const currentTimeElement = document.getElementById("currentTime");
